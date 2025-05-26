c123a09e0913a946734cac7cdc7a1202e719e9de
Datbuihoccode <toro2k3@gmail.com>
2025-05-26 21:29:50 +0700

    Tạo Index + Create

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -1,14 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
+using MvcMovie.Data;
 
 namespace MvcMovie.Controllers
 {
     public class PersonController : Controller
-    {
-        public IActionResult Index()
+    { 
+        private readonly ApplicationDbContext _context;
+        public PersonController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await _context.Person.ToListAsync();
+            return View(model);
+        }
+        public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Person p)
+        {
+            await _context.AddAsync(p);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
